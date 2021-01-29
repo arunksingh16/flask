c37f660b227ba731bef61d78e8fca6956205cff8
singh <singh@mac>
2021-01-29 17:38:11 +0000

    updated for dummy

--- app.py
+++ app.py
@@ -13,39 +13,15 @@ import sqlite3
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your secret key'
 
-def get_db_connection():
-    conn = sqlite3.connect('database.db')
-    conn.row_factory = sqlite3.Row
-    return conn
-
-def get_post(post_id):
-    conn = get_db_connection()
-    post = conn.execute('SELECT * FROM posts WHERE id = ?',(post_id,)).fetchone()
-    conn.close()
-    if post is None:
-        abort(404)
-    return post
-
 
 # flask creare URL map for this
 @app.route('/')
 def indexroot():
-    conn = get_db_connection()
-    posts = conn.execute('SELECT * FROM posts').fetchall()
-    conn.close()
-    return render_template('index.html',mess="Kubernetes", posts=posts)
+    return render_template('index.html',mess="Kubernetes")
     
 @app.route('/index.html')
 def index():
-    conn = get_db_connection()
-    posts = conn.execute('SELECT * FROM posts').fetchall()
-    conn.close()
-    return render_template('index.html',mess="Kubernetes", posts=posts)
-
-@app.route('/<int:post_id>')
-def post(post_id):
-    post = get_post(post_id)
-    return render_template('post.html', post=post)
+    return render_template('index.html',mess="Kubernetes")
 
 
 @app.route('/about.html')

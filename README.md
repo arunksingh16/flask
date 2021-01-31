b1123318763c31a355d37ef9fbbdcc75bd6d29a3
singh <singh@mac>
2021-01-31 20:00:05 +0000

    Weather Api

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # flask
 DUMMY APP FOR DEVOPS TESTING
+
+### Docker Image 
+https://hub.docker.com/r/tiangolo/uwsgi-nginx-flask
+
+http://api.openweathermap.org/data/2.5/weather?q=city_name&APPID=cd0325748b38f32f76d8f2b73dfb59df
--- app.py
+++ app.py
@@ -4,7 +4,10 @@
 
 from flask import Flask, render_template, request, url_for, flash, redirect
 from werkzeug.exceptions import abort
+import configparser
 import sqlite3
+import requests
+
 # import logging
 
 # Logging configuration
@@ -12,7 +15,6 @@ import sqlite3
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your secret key'
-
 
 # flask creare URL map for this
 @app.route('/')
@@ -36,9 +38,25 @@ def dblinks():
 def monitor():
     return render_template('monitor.html')
 
-@app.route('/work.html')
-def work():
-    return render_template('work.html')
+@app.route('/weather.html')
+def weather():
+    return render_template('weather.html')
+
+@app.route('/post.html',methods=['POST'])
+def post():
+    city = request.form['city']
+    API_KEY = 'ce619ba6ee4b5979b052381875ebe756'
+    url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&APPID={API_KEY}'
+    response = requests.get(url).json()
+    current_temperature = response.get('main', {}).get('temp')
+    if current_temperature:
+        current_temperature_celsius = round(current_temperature - 273.15, 2)
+        return render_template('post.html',city=city.title(),temperature=current_temperature_celsius)
+        
+    else:
+        return render_template('post.html',mess="Something is wrong")
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
+    # app.run(host,port,debug,options)
+
